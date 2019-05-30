8614a7edae836facfb033b5c0fbaaa27437f91a7
Nicholas Jackson <nicholasjackson827@gmail.com>
2019-05-29 21:17:10 -0400

    Initial commit

---
+++ src/test/java/com/ngjackson/jegy/JegyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ngjackson.jegy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JegyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
